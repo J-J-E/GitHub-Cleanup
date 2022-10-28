320d650bf3824d70f04863e43df00e0667cc45f3
J-J-E <John.j.Ehlen@gmail.com>
2022-10-28 09:13:11 -0700

    Updated code
    Use main instead of master
    Use switch instead of checkout to avoid confusion

--- GitHub-Branch-Cleanup.sh
+++ GitHub-Branch-Cleanup.sh
@@ -1,23 +1,23 @@
 # This has to be run from master
-git checkout master
+git switch main
 
 # Update our list of remotes
 git fetch
 git remote prune origin
 
 # Remove local fully merged branches
-git branch --merged master | grep -v 'master$' | xargs git branch -d
+git branch --merged main | grep -v 'main$' | xargs git branch -d
 
 # Show remote fully merged branches
 echo "The following remote branches are fully merged and will be removed:"
-git branch -r --merged master | sed 's/ *origin\///' | grep -v 'master$'
+git branch -r --merged main | sed 's/ *origin\///' | grep -v 'main$'
 
 read -p "Continue (y/n)? "
 if [ "$REPLY" == "y" ]
 then
    # Remove remote fully merged branches
-   git branch -r --merged master | sed 's/ *origin\///' \
-             | grep -v 'master$' | xargs -I% git push origin :%
+   git branch -r --merged main | sed 's/ *origin\///' \
+             | grep -v 'main$' | xargs -I% git push origin :%
    echo "Done!"
    say "Obsolete branches are removed"
 fi
